46dbeba55e4c12a2501f1be2cae9b3fe9300dea5
James Dadabo <jamesddadabo@gmail.com>
2026-07-04 13:23:47 -0700

    Better markdown

---
+++ lua/plugins/markview.lua
@@ -0,0 +1,10 @@
+return {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+    keys = {
+      { "<leader>mt", "<cmd>Markview Toggle<cr>" },
+    }
+
+    -- Completion for `blink.cmp`
+    -- dependencies = { "saghen/blink.cmp" },
+};

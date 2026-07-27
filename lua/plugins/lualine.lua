c3a345ba84c61513f57753a3785beffd889eb99f
James Dadabo <jamesddadabo@gmail.com>
2026-07-27 15:06:17 -0700

    Harpoon Files in Lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,19 @@
 return {
   'nvim-lualine/lualine.nvim',
-  opts = true,
   dependencies = {
     'nvim-mini/mini.icons',
+    {
+      'kiennt63/harpoon-files.nvim',
+      dependencies = {
+        { 'ThePrimeagen/harpoon', branch = 'harpoon2' },
+      }
+    }
   },
+  config = function()
+    require('lualine').setup({
+      sections = {
+        lualine_c = { 'filename', require('harpoon_files').lualine_component }
+      }
+    })
+  end,
 }

96c158599aa3e9cb75feab3a44b0a698f5cf331f
James Dadabo <jamesddadabo@gmail.com>
2026-08-18 17:56:41 -0700

    Harpoon files

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,16 +3,38 @@ return {
   dependencies = {
     'nvim-mini/mini.icons',
     {
-      'kiennt63/harpoon-files.nvim',
+      "letieu/harpoon-lualine",
       dependencies = {
-        { 'ThePrimeagen/harpoon', branch = 'harpoon2' },
-      }
+        {
+          "ThePrimeagen/harpoon",
+          branch = "harpoon2",
+        }
+      },
     }
   },
   config = function()
+    -- harpoon-lualine caps the marks it shows at #indicators, so build a
+    -- fixed-size list of formatters that render the mark's file name.
+    local function indicators(fmt)
+      local list = {}
+      for i = 1, 9 do
+        list[i] = function(item)
+          return fmt(i, vim.fn.fnamemodify(item.value, ':t'))
+        end
+      end
+      return list
+    end
+
     require('lualine').setup({
       sections = {
-        lualine_c = { 'filename', require('harpoon_files').lualine_component }
+        lualine_c = {
+          'filename',
+          {
+            'harpoon2',
+            indicators = indicators(function(i, name) return i .. ' ' .. name end),
+            active_indicators = indicators(function(i, name) return '[' .. i .. ' ' .. name .. ']' end),
+          },
+        }
       }
     })
   end,
